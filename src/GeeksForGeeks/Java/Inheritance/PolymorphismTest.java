316fbcb58531d231c3b565d4a66b677e6bbb95ee
Nikhil Bagde <nbagde1@binghamton.edu>
2017-01-08 08:08:39 -0500

    GeekForGeek Java Review

---
+++ src/GeeksForGeeks/Java/Inheritance/PolymorphismTest.java
@@ -0,0 +1,31 @@
+package GeeksForGeeks.Java.Inheritance;
+
+/**
+ * Created by Nikhil on 1/8/2017 7:39 AM.
+ */
+
+class E{
+    public void fun1(){
+        System.out.println("E fun1");
+    }
+}
+
+class F extends E{
+    public void fun1(){
+        System.out.println("F fun1");
+        //to call above fun1 -> super.fun1();
+        super.fun1();
+    }
+}
+public class PolymorphismTest {
+    public static void main(String[] args) {
+        E e = new E();
+        e.fun1();       //E fun1
+
+        E ef = new F(); //instance methods belongs to object getting created. new F() is getting created. Hence it will go with fun1() if F.
+        ef.fun1();      //F fun1
+
+        F f = new F();
+        f.fun1();       // F fun1
+    }
+}
---
+++ src/GeeksForGeeks/Java/Inheritance/StaticShadowing.java
@@ -0,0 +1,25 @@
+package GeeksForGeeks.Java.Inheritance;
+
+/**
+ * Created by Nikhil on 1/8/2017 7:32 AM.
+ */
+
+
+class C{
+    static void function1(){
+        System.out.println("C Static function1");
+    }
+}
+class D extends C{
+    static void function1(){
+        System.out.println("D Static function1");
+    }
+}
+public class StaticShadowing {
+    public static void main(String[] args) {
+        C cd = new D();
+        C.function1();
+        D.function1();
+
+    }
+}
